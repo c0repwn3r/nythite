ddb7f22ab74ca57c01d7d5dafc43dc1e51438560
c0repwn3r <coredoescode@gmail.com>
2021-12-31 17:05:43 -0500

    Add new tools and change wordlist system to be more versatile

--- tools/src/genwordlist.py
+++ tools/src/genwordlist.py
@@ -1,15 +1,16 @@
 import random
+import sys
 
 initialwordlist = []
 wordlist = []
 
-with open('basewordlist.txt') as f:
+with open('meta/basewordlist.txt') as f:
     initialwordlist = f.read().split('\n')
 
 wordlist = random.sample(initialwordlist, 32767)
 if len(wordlist) != len(set(wordlist)):
     print('Duplicates')
 
-with open('wordlist.txt', 'w') as f:
+with open(f'../wordlists/{sys.argv[1]}.txt', 'w') as f:
     for i in wordlist:
         f.write(i + '\n')
